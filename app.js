14ba1a160b49618dbe3da558cf50e97259dbb0cd
Liam Matthews <mattheli@me.com>
2018-08-30 17:43:02 +0100

    #add logger

--- app.js
+++ app.js
@@ -1,8 +1,44 @@
-const fs = require('fs');
+
 
 // fs.readdir
 // Read Directory
+const fs = require('fs');
+
 fs.readdir('./', function(err, files) {
     if(err) console.log('Error', err)
     else console.log('Result', files);
 })
+
+
+// EventEmitter
+const Logger = require('./logger');
+const logger = new Logger();
+
+// register a listener
+// we could equally use emitter.addListener which is exactly the same as emitter.on
+// we have used an arrow function, we could rewrite '(e) => {' as 'function(e){'
+logger.on('messageLogged', (e) => {
+    console.log('Listener Called', e);
+})
+
+logger.log('message');
+
+
+//HTTP
+const http = require('http');
+const server = http.createServer((req, res) => {
+    if(req.url === '/') {
+        res.write('Hello World');
+        res.end();
+    }
+
+    if(req.url === '/api/courses') {
+        res.write(JSON.stringify([1, 2, 3]));
+        res.end();
+    }
+});
+
+
+server.listen(3000);
+
+console.log('Listening on Port 3000...');
--- logger.js
+++ logger.js
@@ -1,8 +1,16 @@
+// notice that we are asigning the constant a class so we use PascalCase.
+const EventEmitter = require('events');
 
-// log a message
-function log(message) {
-    console.log(message);
+class Logger extends EventEmitter {
+
+    // log a message
+    log(message) {
+        console.log(message);
+
+        // raise an event and pass in some data
+        this.emit('messageLogged', { id: '1', url: 'http://mattheli.com'});
+    }
 }
 
 // export log function
-module.exports = log;
+module.exports = Logger;
